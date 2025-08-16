b97bc40c6d49a32e0494dad6f2212b2b4e4aa43d
Benedict Lee <ailnk@naver.com>
2025-08-16 22:57:57 +0900

    feat(template): monorepo-vite
    - Modify to depend on packages (shadcn/ui, eslint-config, tsconfig)
    - Define eslint-config and tsconfig for vite

--- templates/monorepo-vite/apps/web/src/App.tsx
+++ templates/monorepo-vite/apps/web/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button } from "@workspace/ui/components/button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Count is {count}</h1>
+        <Button size="sm" onClick={() => setCount((count) => count + 1)}>
+          button
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
