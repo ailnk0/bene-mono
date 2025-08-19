02ee33193bcb1f76dd896da1183ecae8f3be50b1
Benedict Lee <ailnk@naver.com>
2025-08-19 23:27:22 +0900

    chore(config): change eslint/typescript config and format code

--- packages/bene-mono-cli/src/index.ts
+++ packages/bene-mono-cli/src/index.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { init } from './commands/init';
+import { init } from '@/src/commands/init';
 
-import packageJson from '../package.json';
+import packageJson from '@/package.json';
 
 process.on('SIGINT', () => process.exit(0));
 process.on('SIGTERM', () => process.exit(0));
