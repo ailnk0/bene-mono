699fd433ce9a56dcb899c1de949b3f8689deca27
Benedict Lee <ailnk@naver.com>
2025-08-21 07:57:12 +0900

    fix(template): remove eslint-import-sort-config from recommended config

--- templates/turbo2-vite7-react19-tailwind4-shadcn/packages/eslint-config/eslint.config.js
+++ templates/turbo2-vite7-react19-tailwind4-shadcn/packages/eslint-config/eslint.config.js
@@ -1,14 +1,7 @@
-import importSortConfig from './eslint-import-sort-config.js';
 import tanstackQueryConfig from './eslint-tanstack-query-config.js';
 import turboConfig from './eslint-turbo-config.js';
 import uiConfig from './eslint-ui-config.js';
 import viteConfig from './eslint-vite-config.js';
 
 /** @type {import("eslint").Linter.Config} */
-export default [
-  ...turboConfig,
-  ...viteConfig,
-  ...importSortConfig,
-  ...tanstackQueryConfig,
-  ...uiConfig,
-];
+export default [...turboConfig, ...viteConfig, ...tanstackQueryConfig, ...uiConfig];
