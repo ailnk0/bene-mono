e88cc8867d9bfc0ae852fffd95f2f26c6330f9ac
Benedict Lee <ailnk@naver.com>
2025-08-21 08:08:31 +0900

    docs(template): update README.md for eslint

--- templates/turbo2-vite7-react19-tailwind4-shadcn/apps/web/eslint.config.js
+++ templates/turbo2-vite7-react19-tailwind4-shadcn/apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import config from '@workspace/eslint-config/recommanded';
+import baseConfig from '@workspace/eslint-config/base';
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default baseConfig;
