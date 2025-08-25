8828e032e60d031f63f7a3704934baabfa46a029
Benedict Lee <ailnk8591@gmail.com>
2025-08-25 19:25:00 +0900

    fix(template-vite): Excluding non-essential config

--- templates/turbo2-vite7-react19-tailwind4-shadcn/packages/eslint-config/eslint-tanstack-query-config.js
+++
@@ -1,4 +0,0 @@
-import pluginQuery from '@tanstack/eslint-plugin-query';
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [...pluginQuery.configs['flat/recommended']];
--- templates/turbo2-vite7-react19-tailwind4-shadcn/packages/eslint-config/eslint.config.js
+++ templates/turbo2-vite7-react19-tailwind4-shadcn/packages/eslint-config/eslint.config.js
@@ -1,7 +1,6 @@
-import tanstackQueryConfig from './eslint-tanstack-query-config.js';
 import turboConfig from './eslint-turbo-config.js';
 import uiConfig from './eslint-ui-config.js';
 import viteConfig from './eslint-vite-config.js';
 
 /** @type {import("eslint").Linter.Config} */
-export default [...turboConfig, ...viteConfig, ...tanstackQueryConfig, ...uiConfig];
+export default [...turboConfig, ...viteConfig, ...uiConfig];
